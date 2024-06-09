782eaf01b6466880628cf2698612a6152e8a24ad
Miroslav Bradic <mbradic@yahoo.com>
2024-06-09 19:22:34 +0200

    presentation - intro - app-structure - package.json final

--- presentation/src/intro/AppStructure.js
+++ presentation/src/intro/AppStructure.js
@@ -13,6 +13,7 @@ export default function AppStructure() {
   const [firstRunOutput, setFirstRunOutput] = useState("");
   const [htmlOutput, setHtmlOutput] = useState("");
   const [domOutput, setDomOutput] = useState("");
+  const [packageJsonFinal, setPackageJsonFinal] = useState("");
 
   useEffect(() => {
     async function fetchText(url) {
@@ -67,26 +68,14 @@ export default function AppStructure() {
           "https://raw.githubusercontent.com/mbradic/course-react1/main/examples/hello-world-dom-output.html"
         )
       );
+
+      setPackageJsonFinal(
+        await fetchText(
+          "https://raw.githubusercontent.com/mbradic/course-react1/main/examples/hello-world/package.json"
+        )
+      );
     })();
   }, []);
-
-  //   const index_js = `import React from 'react';
-  // import ReactDOM from 'react-dom/client';
-  // import './index.css';
-  // import App from './App';
-  // import reportWebVitals from './reportWebVitals';
-
-  // const root = ReactDOM.createRoot(document.getElementById('root'));
-  // root.render(
-  //   <React.StrictMode>
-  //     <App />
-  //   </React.StrictMode>
-  // );
-
-  // // If you want to start measuring performance in your app, pass a function
-  // // to log results (for example: reportWebVitals(console.log))
-  // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  // reportWebVitals();`;
 
   const babel_in = `const title = <h1>Hello world</h1>;`;
 
