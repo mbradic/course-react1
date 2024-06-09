02b425c58ae426e40cdb9fe8aac27641a7655bb9
Miroslav Bradic <mbradic@yahoo.com>
2024-06-09 12:28:02 +0200

    app structure package.json

--- presentation/src/intro/AppStructure.js
+++ presentation/src/intro/AppStructure.js
@@ -2,47 +2,60 @@ import ReactCodeMirror from "@uiw/react-codemirror";
 import { json } from "@codemirror/lang-json";
 import { javascript } from "@codemirror/lang-javascript";
 import { html } from "@codemirror/lang-html";
+import { useEffect, useState } from "react";
 
-export default function JSFramework() {
-  const package_json = `{
-    "name": "hello-world",
-    "version": "0.1.0",
-    "private": true,
-    "dependencies": {
-      "@testing-library/jest-dom": "^5.17.0",
-      "@testing-library/react": "^13.4.0",
-      "@testing-library/user-event": "^13.5.0",
-      "react": "^18.2.0",
-      "react-dom": "^18.2.0",
-      "react-scripts": "5.0.1",
-      "web-vitals": "^2.1.4"
-    },
-    "scripts": {
-      "start": "react-scripts start",
-      "build": "react-scripts build",
-      "test": "react-scripts test",
-      "eject": "react-scripts eject"
-    },
-    "eslintConfig": {
-      "extends": [
-        "react-app",
-        "react-app/jest"
-      ]
-    },
-    "browserslist": {
-      "production": [
-        ">0.2%",
-        "not dead",
-        "not op_mini all"
-      ],
-      "development": [
-        "last 1 chrome version",
-        "last 1 firefox version",
-        "last 1 safari version"
-      ]
-    }
-  }
-  `;
+export default function AppStructure() {
+  const [packageJsonBefore, setPackageJsonBefore] = useState('')
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/mbradic/course-react1/main/examples/hello-world/package.json"
+      );
+      const text = await response.text();
+      setPackageJsonBefore(text);
+    })();
+  }, []);
+  // const package_json = `{
+  //   "name": "hello-world",
+  //   "version": "0.1.0",
+  //   "private": true,
+  //   "dependencies": {
+  //     "@testing-library/jest-dom": "^5.17.0",
+  //     "@testing-library/react": "^13.4.0",
+  //     "@testing-library/user-event": "^13.5.0",
+  //     "react": "^18.2.0",
+  //     "react-dom": "^18.2.0",
+  //     "react-scripts": "5.0.1",
+  //     "web-vitals": "^2.1.4"
+  //   },
+  //   "scripts": {
+  //     "start": "react-scripts start",
+  //     "build": "react-scripts build",
+  //     "test": "react-scripts test",
+  //     "eject": "react-scripts eject"
+  //   },
+  //   "eslintConfig": {
+  //     "extends": [
+  //       "react-app",
+  //       "react-app/jest"
+  //     ]
+  //   },
+  //   "browserslist": {
+  //     "production": [
+  //       ">0.2%",
+  //       "not dead",
+  //       "not op_mini all"
+  //     ],
+  //     "development": [
+  //       "last 1 chrome version",
+  //       "last 1 firefox version",
+  //       "last 1 safari version"
+  //     ]
+  //   }
+  // }
+  // `;
+
+
 
   const index_js = `import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -116,12 +129,24 @@ export default App;
       <h3>Struktura aplikace</h3>
       <ul>
         <li>paskage.json a node_modules</li>
+        
+        <p>Po vytvoření:</p>
         <ReactCodeMirror
-          value={package_json}
+          value={packageJsonBefore}
           readOnly
           style={{ margin: "15px 0px 15px 0px" }}
           extensions={[json()]}
         />
+        <p>Po úpravě:</p>
+        <ReactCodeMirror
+          value={packageJsonBefore}
+          readOnly
+          style={{ margin: "15px 0px 15px 0px" }}
+          extensions={[json()]}
+          
+        />
+        
+        
         <li>src/index.js</li>
 
         <ReactCodeMirror
--- presentation/src/intro/DevEnv.js
+++ presentation/src/intro/DevEnv.js
@@ -1,13 +1,24 @@
 import ReactCodeMirror from "@uiw/react-codemirror";
+import { useEffect, useState } from "react";
 
 export default function DevEnv() {
+  const [createHelloWorldBat, setCreateHelloWorldBat] = useState("");
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/mbradic/course-react1/main/examples/create-hello-world.bat"
+      );
+      const text = await response.text();
+      setCreateHelloWorldBat(text);
+    })();
+  }, []);
   return (
     <>
       <h3>Vývojové prostředí</h3>
       <ul>
         <li>node.js</li>
         <li>npm</li>
-        <li style={{textDecoration: "line-through"}}>create-react-app</li>
+        <li style={{ textDecoration: "line-through" }}>create-react-app</li>
         <ReactCodeMirror
           value="npx create-react-app hello-world"
           readOnly
@@ -16,7 +27,7 @@ export default function DevEnv() {
         />
         <li>
           <ReactCodeMirror
-            value={`md hello-world\ncd hello-world\nnpm init -y\nnpm i react-dom\nnpm i -D react-scripts`}
+            value={createHelloWorldBat}
             readOnly
             style={{ margin: "15px 0px 15px 0px" }}
             basicSetup={{ lineNumbers: false }}
