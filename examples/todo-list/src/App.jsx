3361c1c7ab90a98ce8371b4f8caffe29d6f65258
mbradic <mbradic@yahoo.com>
2024-06-12 12:28:18 +0200

    Update App.jsx

--- examples/todo-list/src/App.jsx
+++ examples/todo-list/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function App() {
-  const initialCount = 0;
   const [todo, setToDo] = useState({
     task: "Naučit se prezentovat data v Reactu",
     done: false,
