0923bb84d9fc9687b23f4e576eab2699eb9f8d5e
Miroslav Bradic <mbradic@yahoo.com>
2024-11-26 22:21:53 +0100

    Added tasks to todos

--- examples/todo-list/src/App.jsx
+++ examples/todo-list/src/App.jsx
@@ -1,19 +1,56 @@
 import { useState } from "react";
 
 function App() {
-  const initialCount = 0;
-  const [todo, setToDo] = useState({
-    task: "Naučit se prezentovat data v Reactu",
-    done: false,
-  });
-  const handleClick = () => setToDo((old) => ({ ...old, done: true }));
+  const [todos, setToDos] = useState([
+    {
+      id: 1,
+      task: "Naučit se prezentovat data v Reactu",
+      done: true,
+    },
+    {
+      id: 2,
+      task: "Naučit se reagovat na události",
+      done: true,
+    },
+    {
+      id: 3,
+      task: "Naučit se inputy a formuláře v Reactu",
+      done: false,
+    },
+    {
+      id: 4,
+      task: "Naučit se komunikovat s back-endem",
+      done: false,
+    },
+  ]);
+  const handleDone = (id) =>
+    setToDos((old) => old.map((o) => (o.id !== id ? o : { ...o, done: true })));
   return (
     <>
-      <div style={{ textDecoration: todo.done ? "line-through" : "" }}>
-        Task: {todo.task}
-      </div>
-      <div>Done: {todo.done ? "Yes" : "No"}</div>
-      <button onClick={handleClick}>Done</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Task</th>
+            <th>Done</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((t) => (
+            <tr key={t.id}>
+              <td>{t.id}</td>
+              <td>{t.task}</td>
+              <td>{t.done ? "Yes" : "No"}</td>
+              <td>
+                {!t.done && (
+                  <button onClick={() => handleDone(t.id)}>Done</button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
